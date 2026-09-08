d14d85398344e304f3f85cf1c1ffe6505d6a90b6
José Galdámez <josegaldamez1991@gmail.com>
2026-09-08 14:47:14 -0600

    remove rust version

--- rust-old/src-tauri/migrations/1_init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS projects (
-  uid TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  description TEXT,
-  iconColor TEXT,
-  lastOpenAt DATETIME,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
